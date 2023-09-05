42ab7d250c969820f8b58506667035dc62b8be1d
dhivyabharathyt <dhivyabharathyt@gmail.com>
2023-09-05 12:28:09 -0500

    Second commit

---
+++ NewJP/src/NewPK/abcClass.java
@@ -0,0 +1,13 @@
+package NewPK;
+
+public class abcClass {
+	public static void main(String args[])
+	{
+		String hungry="yes";
+		if(
+				hungry=="yes")
+			System.out.println("I am hungry");
+		else
+			System.out.println("I am not hungry");
+		}
+}
