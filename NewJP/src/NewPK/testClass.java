0d4f8244cf35a273b1deaf6697a1c6aca772ff84
dhivyabharathyt <dhivyabharathyt@gmail.com>
2023-09-05 11:16:40 -0500

    First commit

---
+++ NewJP/src/NewPK/testClass.java
@@ -0,0 +1,13 @@
+package NewPK;
+
+public class testClass {
+public static void main(String args[])
+{
+	String hungry="yes";
+	if(
+			hungry=="yes")
+		System.out.println("I am hungry");
+	else
+		System.out.println("I am not hungry");
+	}
+}
